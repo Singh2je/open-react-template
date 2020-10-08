4d3988b93db36b1ab76d81547ca6a008bf003f62
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2020-10-07 22:25:39 -0400

    commit on 'master'

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 //import './App.css';
 import './assets/scss/style.scss';
@@ -18,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
